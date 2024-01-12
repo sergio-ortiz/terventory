511db7d198a2dbbdabe3f978d886e78c7f6c397b
Sergio <ortiz.sergio0303@gmail.com>
2024-01-12 15:04:24 -0500

    feat(config): tailwindcss to use daisyui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1 class="mx-5 mt-5 text-xl text-orange-500">Welcome to SvelteKit</h1>
-<p class="mx-5">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<article class="prose m-5">
+	<h1 class="text-orange-500">Welcome to SvelteKit</h1>
+	<p class="">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<button class="btn btn-primary">click me</button>
+</article>
