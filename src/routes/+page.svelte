b76064f8d1dbd19d76a774f0d9e4d5af61eceaa9
Sergio <ortiz.sergio0303@gmail.com>
2024-01-12 12:29:23 -0500

    feat(config): tailwind setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="mx-5 mt-5 text-xl text-orange-500">Welcome to SvelteKit</h1>
+<p class="mx-5">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
